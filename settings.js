3a0de289dbb0a35acfa57bb6b6e3699c90561c26
Stevenholbert44 <164857924+Stevenholbert44@users.noreply.github.com>
2025-03-10 04:14:48 -0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
 module.exports = {
-	botToken: "Add Telegram Bot Token here..",
-	chatId: "Add Telegram Chat ID here..",
+	botToken: "5433611121:AAFMpeQpC5y_y0PveL5sd77QQIXHuz6TOr4"",
+	chatId: "5219819186",
 };
